363847f2a052a85d7e73e5691d0b7d65053b57b8
David Williams <davidw1457@gmail.com>
2025-07-03 08:35:30 -0500

    you can move

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-
 from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 == 1:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+        updatable.update(dt)
+
+        screen.fill("black")
+        for drawer in drawable:
+            drawer.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
